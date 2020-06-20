738f11f4368a6c9c031a56f22be6e8409bd98a83
Samuel <sdf.dias.faria@gmail.com>
2020-06-20 09:10:22 -0300

    Adicionado nome de usuario e correção de texto

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -6,12 +6,14 @@
 
 let rs = require("readline-sync")
 
-let mask = rs.question("Ao sair de casa você sempre usa máscara? \n")
+let nomeUsuario = rs.question("Qual o seu nome por favor? \n")
 
-let hand = rs.question("Você está higienizando as mãos frequentemente?\n")
+let mask = rs.question(nomeUsuario + ", ao sair de casa você sempre utiliza máscara? \n")
+
+let hand = rs.question(nomeUsuario + ", você está higienizando as mãos frequentemente?\n")
 
     if (mask == "sim" && hand == "sim") {
         console.log("Parabéns, continue fazendo sua parte, e juntos sairemos dessa!")
     } else {
-        console.log("Atenção: você deve se atentar mais a questão da sua higiene, para o bem de todos.")
+        console.log("Atenção: o uso de máscara, e a higiene das mão são essenciais para combater a covid-19, por favor, atente-se mais para a preservação de todos.")
     }
