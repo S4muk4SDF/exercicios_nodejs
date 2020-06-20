fd8c0184a03600498b87807c13bd5f3491b200bf
Samuel <sdf.dias.faria@gmail.com>
2020-06-19 22:04:02 -0300

    Atividade 1 resolvida

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,13 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require("readline-sync")
 
 // ## Faça o código ##
+
+let nome = rs.question("Qual o seu nome? \n")
+
+let numberVoo = rs.questionInt("Olá " + nome + ", qual o número do seu vôo? \n")
+
+let fileira = rs.question("Qual a sua fileira? \n")
+
+console.log(nome + ", seu número de vôo é " + numberVoo + ", e sua fileira é a " + fileira + ".")
