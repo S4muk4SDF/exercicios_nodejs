ce1204213425eea461b457f0bba7eb4915b51dad
Samuel <sdf.dias.faria@gmail.com>
2020-06-20 09:30:12 -0300

    Adicionado nome de usuario

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,11 +1,11 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require("readline-sync")
 
 // ## Faça o código ##
 
-let rs = require("readline-sync")
+let nomeUsuario = rs.question("Digite seu nome por favor. \n")
 
 let text = rs.question("Digite qualquer coisa \n")
 
-console.log("Você digitou " + text.length + " caracteres") 
+console.log(nomeUsuario + ", você digitou " + text.length + " caracteres") 
