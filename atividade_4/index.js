f0f99af83629096bb802a673f1e77641e3faea33
Samuel <sdf.dias.faria@gmail.com>
2020-06-20 09:26:35 -0300

    Adicionado nome de usuario e correção de texto

--- atividade_4/index.js
+++ atividade_4/index.js
@@ -1,10 +1,10 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require("readline-sync")
 
 // ## Faça o código ##
 
-let rs = require("readline-sync")
+let nomseusuario = rs.question("Digite seu nome por favor. \n")
 
 let day = rs.question("Qual o seu dia de aniversário? \n")
 
@@ -14,49 +14,49 @@ let month = rs.question("Qual o seu mês de aniversário? \n")
 
 
 if (day >=21 && month == "março" ||  day <=20 && month == "abril") {
-    console.log("Seu signo é Áries")
+    console.log(nomseusuario + ", seu signo é Áries")
 } 
 
 else if (day >=21 && month == "abril" || day <=20 && month == "maio") {
-    console.log("Seu signo é Touro")
+    console.log(nomseusuario + ", seu signo é Touro")
 } 
 
 else if (day >=21 && month == "maio" || day <=20 && month == "junho") {
-    console.log("Seu signo é Gêmeos")
+    console.log(nomseusuario + ", seu signo é Gêmeos")
 } 
 
 else if (day >=21 && month == "junho" || day <=22 && month == "julho") {
-    console.log("Seu signo é Câncer")
+    console.log(nomseusuario + ", seu signo é Câncer")
 } 
 
 else if (day >=23 && month == "julho" || day <=22 && month == "agosto") {
-    console.log("Seu signo é Leão")
+    console.log(nomseusuario + ", seu signo é Leão")
 } 
 
 else if (day >=23 && month == "agosto" || day <=22 && month == "setembro") {
-    console.log("Seu signo é Virgem")
+    console.log(nomseusuario + ", seu signo é Virgem")
 } 
 
 else if (day >=23 && month == "setembro" || day <=22 && month == "outubro") {
-    console.log("Seu signo é Libra")
+    console.log(nomseusuario + ", seu signo é Libra")
 } 
 
 else if (day >=23 && month == "outubro" || day <=21 && month == "novembro") {
-    console.log("Seu signo é Escorpião")
+    console.log(nomseusuario + ", seu signo é Escorpião")
 } 
 
 else if (day >=22 && month == "novembro" || day <=21 && month == "dezembro") {
-    console.log("Seu signo é Sagitário")
+    console.log(nomseusuario + ", seu signo é Sagitário")
 } 
 
 else if (day >=22 && month == "dezembro" || day <=20 && month == "janeiro") {
-    console.log("Seu signo é Capricórnio")
+    console.log(nomseusuario + ", seu signo é Capricórnio")
 } 
 
 else if (day >=21 && month == "janeiro" || day <=18 && month == "fevereiro") {
-    console.log("Seu signo é Aquário")
+    console.log(nomseusuario + ", seu signo é Aquário")
 } 
 
 else {
-    console.log("Seu signo é Peixes")
+    console.log(nomseusuario + ", seu signo é Peixes")
 }
